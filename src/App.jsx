5d5c03f24baf8215037c00887e96e0b4dda24961
Simran Singh <simransingh25060@gmail.com>
2025-01-12 03:28:53 +0530

    added hooks

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+     <h1 className='bg-green-400'> hello </h1>
+    </>
+  )
+}
+
+export default App
